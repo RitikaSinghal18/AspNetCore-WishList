10498817266bdfc0a920751a738188fb36abc209
Ritika Singhal <RSinghal@AffinionDS.Com>
2022-10-28 14:20:08 +0530

    create home views and the homecontroller

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: HomeController
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+       
+    }
+}
